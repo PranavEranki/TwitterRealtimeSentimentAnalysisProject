7b9c32644fdd090a1678b0a00fad410bcb7da57b
PranavEranki <pranav.eranki@gmail.com>
2019-03-31 12:26:49 -0700

    3/30/19 work + some 3/31/19 work

---
+++ fullModelTraining.py
@@ -0,0 +1,102 @@
+import string
+import re
+
+import pandas as pd
+import numpy as np
+
+from nltk.corpus import twitter_samples
+from nltk.tokenize import TweetTokenizer
+from nltk.stem import SnowballStemmer
+from nltk.tokenize import TweetTokenizer
+from nltk.corpus import stopwords
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import f1_score
+from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+
+def import_tweets(filename, frac, header = None):
+	#import data from csv file via pandas library.
+    # Only take a certain fraction of the data. We do not need 1.6 million tweets
+	tweet_dataset = pd.read_csv(filename, encoding = "ISO-8859-1").sample(frac = frac)
+	#the column names are based on sentiment140 dataset provided on kaggle
+	tweet_dataset.columns = ['sentiment','id','date','flag','user','text']
+	#delete 3 columns: flags,id,user, as they are not required for analysis
+	for i in ['flag','id','user','date']: del tweet_dataset[i] # or tweet_dataset = tweet_dataset.drop(["id","user","date","user"], axis = 1)
+	#in sentiment140 dataset, positive = 4, negative = 0; So we change positive to 1
+	tweet_dataset.sentiment = tweet_dataset.sentiment.replace(4,1)
+	return tweet_dataset
+
+# Now, we need to 'clean' the tweet
+def cleanTweets(tweet):
+    tweet.lower()
+    #convert all urls to sting "URL"
+    tweet = re.sub('((www\.[^\s]+)|(https?://[^\s]+))' , ' ', tweet)
+    #convert all @username to "AT_USER"
+    tweet = re.sub('@[^\s]+',' ', tweet)
+    #correct all multiple white spaces to a single white space
+    tweet = re.sub('[\s]+', ' ', tweet)
+    #convert "#topic" to just "topic"
+    tweet = re.sub(r'#([^\s]+)', ' ', tweet)
+    return tweet
+
+def useVectorizer(data, max):
+    tfv=TfidfVectorizer(min_df=0, max_features= max, strip_accents='unicode',lowercase =True,
+                            analyzer='word', token_pattern=r'\w{3,}', ngram_range=(1,1),
+                            use_idf=True,smooth_idf=True, sublinear_tf=True, stop_words = "english") # we need to give proper stopwords list for better performance
+    features=tfv.fit_transform(data)
+    # print ("dataset transformed")
+    # print ("dataset shape ", features.shape)
+    return features.toarray()
+
+def train_classifier(x_train, y_train, multiple = False):
+    if (multiple == True):
+        models = [GaussianNB(), MultinomialNB(), SVC(), BernoulliNB()]
+        for model in models:
+            print("Training : " + str(type(model)))
+            model.fit(x_train, y_train)
+        return models
+    else:
+        # Insert optimum model here with optimum parameters
+        model = GaussianNB()
+        # do grid search cv for the best parameters
+
+        return model
+
+def testClassifier(cls, x, y):
+    if isinstance(cls, (list,)):
+        for model in cls:
+            print("For model : " + str(type(model)))
+            pred = model.predict(x)
+            f1 = f1_score(y, pred)
+            cm = confusion_matrix(y, pred)
+            print("F1 score : " + str(f1))
+            print("Confusion matrix : " + str(cm))
+            print()
+    else:
+        pred = cls.predict(x)
+        f1 = f1_score(y, pred)
+        cm = confusion_matrix(y, pred)
+        print("F1 score : " + str(f1))
+        print("Confusion matrix : " + str(cm))
+
+
+def full():
+    tweet_dataset = import_tweets("data/data.csv", 0.006)
+    tweet_dataset['text'] = tweet_dataset['text'].apply(cleanTweets)
+    data = np.array(tweet_dataset.text)
+    label = np.array(tweet_dataset.sentiment)
+    features = useVectorizer(data, 1500)
+    x_train, x_test, y_train, y_test = train_test_split(features, label, test_size=0.25, random_state=0)
+    all_models = train_classifier(x_train, y_train, multiple = True)
+    testClassifier(all_models, x_test, y_test)
+
+if __name__ == "__main__":
+    full()
--- log.md
+++ log.md
@@ -11,5 +11,14 @@
 - Got the data from a kaggle dataset [here](https://www.kaggle.com/kazanova/sentiment140)
 - Python code
   - Wrote a cleaning tweet method using regex, porterstemmer, and tokenizer
-  - Wrote a bag of words method
+  - Created a bag of words using tfidf vectorizer
   - Read the data, processed it, and split it
+  - Tested different vectorizers and different methods
+  - Fixed some bugs in program
+
+# 3/31/19
+- Changed architecture
+- Removed some of the preprocessing that was causing errors
+- Organized code into reusable functions
+- Finalized code for single model
+- Added functionality to test various models to determine optimum model
